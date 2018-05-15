740120b91a01bb7a812c847b31c59904bc8fc68d
Kirill Khudyakov <hudiakov@gmail.com>
2018-05-15 14:20:13 +0300

    Init xcode project

---
+++ FoodApp/FoodApp/Screens/Search/SearchViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FoodApp
+//
+//  Created by Kirill Khudiakov on 14.05.2018.
+//  Copyright © 2018 Kirill Khudiakov. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    convenience init(viewModel: String) {
+        self.init()
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
