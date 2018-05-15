40e4223c9ae85f76c1d4f1ae78f1cde365a49760
Kirill Khudyakov <hudiakov@gmail.com>
2018-05-15 17:02:53 +0300

    Set light color for statusBar

--- FoodApp/FoodApp/AppDelegate.swift
+++ FoodApp/FoodApp/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
+        UIApplication.shared.statusBarStyle = .lightContent
         configureWindow()
         return true
     }
