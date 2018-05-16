5021629c28be90c89a02718a77124d4e2d37978f
Kirill Khudyakov <hudiakov@gmail.com>
2018-05-16 18:28:17 +0300

    Add Food as model

---
+++ FoodApp/FoodApp/Models/Food.swift
@@ -0,0 +1,26 @@
+//
+//  Food.swift
+//  FoodApp
+//
+//  Created by Kirill Khudiakov on 16.05.2018.
+//  Copyright © 2018 Kirill Khudiakov. All rights reserved.
+//
+
+import Foundation
+
+
+
+class FoodImage {
+    var main: String?
+    var small: String?
+}
+
+class Food {
+    var guid: String?
+    var index: Int?
+    var name: String?
+    var description: String?
+    var price: Float?
+    var images: FoodImage?
+    var reviews: Int?
+}
